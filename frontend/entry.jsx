aad1b85f4e76c87a33181598fae4defc4faa6d90
vickiychung <vickiychung@gmail.com>
2021-04-05 14:39:57 -0700

    Add store, root reducer

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
-import { signUp, signIn, signOut } from './util/session_api_util';
+//TESTING
+import { signUp, signIn, signOut } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
+  const store = configureStore();
 
+  //TESTING
   window.signUp = signUp;
   window.signIn = signIn;
   window.signOut = signOut;
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   ReactDOM.render(<h1>Hi I'm in the entry file</h1>, root);
 });
