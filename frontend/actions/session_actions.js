06f273029cc155a2334c3eb7fa409b2221faa74c
vickiychung <vickiychung@gmail.com>
2021-04-05 12:12:00 -0700

    Add session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,63 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const SIGNOUT_CURRENT_USER = "SIGNOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+const receiveCurrentUser = currentUser => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+const signoutCurrentUser = () => {
+  return {
+    type: SIGNOUT_CURRENT_USER,
+  };
+};
+
+const receiveErrors = errors => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
+
+// clearErrors ?
+
+export const signUp = user => {
+  return (dispatch => {
+    return (
+      SessionAPIUtil.signUp(user)
+        .then(
+          currentUser => dispatch(receiveCurrentUser(currentUser)),
+          errors => dispatch(receiveErrors(errors.responseJSON))
+        )
+    );
+  });
+};
+
+export const signIn = user => {
+  return (dispatch => {
+    return (
+      SessionAPIUtil.signIn(user)
+        .then(
+          currentUser => dispatch(receiveCurrentUser(currentUser)),
+          errors => dispatch(receiveErrors(errors.responseJSON))
+        )
+    );
+  });
+};
+
+export const signOut = () => {
+  return (dispatch => {
+    return (
+      SessionAPIUtil.signOut()
+        .then(
+          () => dispatch(signoutCurrentUser()),
+          errors => dispatch(receiveErrors(errors.responseJSON))
+        )
+    );
+  });
+};
