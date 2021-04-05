0e98e2208f55863de6990607379311df1174f65a
vickiychung <vickiychung@gmail.com>
2021-04-05 13:54:16 -0700

    Add users reducer, entities reducer

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_CURRENT_USER, SIGNOUT_CURRENT_USER } from '../actions/session_actions';
+
+const usersReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  let newState = Object.assign({}, oldState);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      newState[action.currentUser.id] = action.currentUser;
+      return newState;
+
+    case SIGNOUT_CURRENT_USER:
+      return {};
+  
+    default:
+      return oldState;
+  }
+};
+
+export default usersReducer;
