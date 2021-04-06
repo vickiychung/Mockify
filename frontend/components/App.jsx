8e0ff5c01d0ee9d573c1bb908936102b2c1d87f8
vickiychung <vickiychung@gmail.com>
2021-04-06 09:16:10 -0700

    Add redirect route to splash

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 import HomeContainer from './home/home_container';
 import SignInFormContainer from '../components/session/sign_in_form_container';
@@ -13,6 +13,7 @@ const App = () => {
         <AuthRoute exact path="/signin" component={SignInFormContainer} />
         <AuthRoute exact path="/signup" component={SignUpFormContainer} />
         <Route exact path="/" component={HomeContainer} />
+        <Redirect to="/" />
       </Switch>
     </div>
   );
