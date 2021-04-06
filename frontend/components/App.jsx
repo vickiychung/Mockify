d9987b40ebf7b78273f9fb87a9098b1e3362cac7
vickiychung <vickiychung@gmail.com>
2021-04-05 17:42:32 -0700

    Make /signin and /signup AuthRoute

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,13 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import HomeContainer from './home/home_container';
 import SignInFormContainer from '../components/session/sign_in_form_container';
 import SignUpFormContainer from '../components/session/sign_up_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => {
   return (
     <div>
       <Switch>
-        <Route exact path="/signin" component={SignInFormContainer} />
-        <Route exact path="/signup" component={SignUpFormContainer} />
+        <AuthRoute exact path="/signin" component={SignInFormContainer} />
+        <AuthRoute exact path="/signup" component={SignUpFormContainer} />
         <Route exact path="/" component={HomeContainer} />
       </Switch>
     </div>
