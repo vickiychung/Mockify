27f6557266df8cc551fcc1e8797ddb9256e82a67
vickiychung <vickiychung@gmail.com>
2021-04-05 17:28:25 -0700

    Bootstrap current user

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -4,20 +4,34 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 //TESTING
-import { signUp, signIn, signOut } from './actions/session_actions';
+// import { signUp, signIn, signOut } from './actions/session_actions';
 //TESTING
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
-  const store = configureStore();
+  let store;
+
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
   //TESTING
-  window.signUp = signUp;
-  window.signIn = signIn;
-  window.signOut = signOut;
+  // window.signUp = signUp;
+  // window.signIn = signIn;
+  // window.signOut = signOut;
 
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
   //TESTING
 
   ReactDOM.render(<Root store={store} />, root);
