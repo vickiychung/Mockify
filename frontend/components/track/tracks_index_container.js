db362fe5b5406211531943a904cf6c4652e476c1
vickiychung <vickiychung@gmail.com>
2021-04-07 16:58:20 -0700

    Add tracks index container

--- frontend/components/track/tracks_index_container.js
+++ frontend/components/track/tracks_index_container.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchTrack } from '../../actions/tracks_actions';
+import { fetchTracks } from '../../actions/tracks_actions';
 import TracksIndex from './tracks_index';
 
-// const mapStateToProps = state => {
-//   return {
-//     track: state.entities.tracks
-//   }
-// }
+const mapStateToProps = state => {
+  return {
+    tracks: Object.values(state.entities.tracks)
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchTracks: () => dispatch(fetchTracks())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TracksIndex);
