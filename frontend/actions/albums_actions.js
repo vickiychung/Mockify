c9d6ba157e6d50438c52ae8c8c77f372bd1af6e1
vickiychung <vickiychung@gmail.com>
2021-04-11 15:11:30 -0700

    Add albums actions

--- frontend/actions/albums_actions.js
+++ frontend/actions/albums_actions.js
@@ -1,3 +1,34 @@
 import * as AlbumsAPIUtil from '../util/albums_api_util';
 
-export const 
+export const RECEIVE_ALBUMS = "RECEIVE_ALBUMS";
+export const RECEIVE_ALBUM = "RECEIVE_ALBUM";
+
+const receiveAlbums = albums => {
+  return {
+    type: RECEIVE_ALBUMS,
+    albums
+  };
+};
+
+const receiveAlbum = album => {
+  return {
+    type: RECEIVE_ALBUM,
+    album
+  };
+};
+
+export const fetchAlbums = () => {
+  return (dispatch => {
+    return (
+      AlbumsAPIUtil.fetchAlbums().then(albums => dispatch(receiveAlbums(albums)))
+    );
+  });
+};
+
+export const fetchAlbum = albumId => {
+  return (dispatch => {
+    return (
+      AlbumsAPIUtil.fetchAlbum(albumId).then(album => dispatch(receiveAlbum(album)))
+    );
+  });
+};
