7d247ca485236d700835efd7bf34dd5932962532
vickiychung <vickiychung@gmail.com>
2021-04-11 15:26:00 -0700

    Set up albums components

---
+++ frontend/components/albums/albums_index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class AlbumsIndex extends React.Component {
+  
+  render() {
+    return null;
+  }
+}
+
+export default AlbumsIndex;
---
+++ frontend/components/albums/albums_index_container.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchAlbums } from '../../actions/albums_actions';
+import AlbumsIndex from './albums_index';
+
+// const mapStateToProps = state => {
+//   return {
+
+//   }
+// }
