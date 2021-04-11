4893a16d7be5a8010802d7b186f824f1e2e3fb6f
vickiychung <vickiychung@gmail.com>
2021-04-11 15:27:04 -0700

    Add albumsReducer to entitiesReducer

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import tracksReducer from './tracks_reducer';
+import albumsReducer from './albums_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
-  tracks: tracksReducer
+  tracks: tracksReducer,
+  albums: albumsReducer
 });
 
 export default entitiesReducer;
