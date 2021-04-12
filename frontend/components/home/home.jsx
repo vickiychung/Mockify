54c933b0883e77b2106f8c11eb4d8d12e3559bb6
vickiychung <vickiychung@gmail.com>
2021-04-12 09:46:55 -0700

    Fix signout bug

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Splash from './splash';
 import SignedInHome from './signed_in_home';
 
 const Home = ({ currentUser, signOut }) => {
   const signedIn = () => {
     return (
-      <SignedInHome />
+      <SignedInHome signOut={signOut}/>
     );
   };
 
