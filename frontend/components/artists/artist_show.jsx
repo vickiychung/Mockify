98809c70c5305b64d4b857f8984561664d210338
vickiychung <vickiychung@gmail.com>
2021-04-11 21:12:24 -0700

    Add artist show page

--- frontend/components/artists/artist_show.jsx
+++ frontend/components/artists/artist_show.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
 class ArtistShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchArtist(this.props.match.params.artistId);
+  }
 
   render() {
-    return (
-      <div>
-        <p>this is artist show page</p>
-      </div>
-    )
+    const { artist, albums, currentUser } = this.props;
+    
+    if (artist) {
+      console.log(albums);
+      return (
+        <div>
+          <p>{currentUser.username}</p>
+
+          <h1>{artist.name}</h1>
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
