2f009c7c9f45b161d238d19b0ade04b600b9f419
vickiychung <vickiychung@gmail.com>
2021-04-11 17:20:05 -0700

    Modify webplayer css

--- frontend/components/albums/albums_index.jsx
+++ frontend/components/albums/albums_index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class AlbumsIndex extends React.Component {
   
   render() {
-    return null;
+    
   }
 }
 
--- frontend/components/albums/albums_index_container.js
+++ frontend/components/albums/albums_index_container.js
@@ -3,8 +3,16 @@ import { connect } from 'react-redux';
 import { fetchAlbums } from '../../actions/albums_actions';
 import AlbumsIndex from './albums_index';
 
-// const mapStateToProps = state => {
-//   return {
+const mapStateToProps = state => {
+  return {
+    albums: Object.values(state.entities.albums)
+  };
+};
 
-//   }
-// }
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchAlbums: () => dispatch(fetchAlbums())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AlbumsIndex);
