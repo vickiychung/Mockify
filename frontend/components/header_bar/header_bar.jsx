c2273f1da48c9932ce78cc6603b60746eb956338
vickiychung <vickiychung@gmail.com>
2021-04-13 12:37:41 -0700

    Make header dropdown menu

--- frontend/components/header_bar/header_bar.jsx
+++ frontend/components/header_bar/header_bar.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 
 class HeaderBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { dropdown: "hidden" }
+
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  toggleDropdown() {
+    if (this.state.dropdown === "show") {
+      this.setState({ dropdown: "hidden" });
+    } else {
+      this.setState({ dropdown: "show" });
+    }
+  }
 
   render() {
     const { currentUser, signOut } = this.props;
 
     return (
-      <div>
-        <p>{currentUser.username}</p>
-        <button onClick={signOut}>Log out</button>
+      <div className="header-bar-wrapper">
+        <button className="header-name" onClick={this.toggleDropdown}>
+          {currentUser.username}
+        </button>
+
+        <div className={`header-dropdown ${this.state.dropdown}`}>
+          <button className="header-logout" onClick={signOut}>Log out</button>
+        </div>
       </div>
     );
   }
