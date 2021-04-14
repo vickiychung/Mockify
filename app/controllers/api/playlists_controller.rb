b857049322e239ddaf7ab32e6144188adafe8d94
vickiychung <vickiychung@gmail.com>
2021-04-13 22:18:21 -0700

    add controller actions and routes for playlists

---
+++ app/controllers/api/playlists_controller.rb
@@ -0,0 +1,53 @@
+class Api::PlaylistsController < ApplicationController
+  def index
+    @playlists = Playlist.find_by(user_id: current_user.id)
+    render :index
+  end
+
+  def show
+    @playlist = Playlist.find_by(id: params[:id])
+
+    if @playlist
+      render :show
+    else
+      render json: @playlist.errors.full_messages, status: 422
+    end
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+
+    if @playlist.save
+      render :show
+    else
+      render json: @playlist.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @playlist = Playlist.find_by(id: params[:id])
+
+    if @playlist.save
+      render :show
+    else
+      render json: @playlist.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @playlist = Playlist.find_by(id: params[:id])
+
+    if @playlist
+      @playlist.destroy
+      render json: @playlist.id
+    else
+      render json: @playlist.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:user_id, :name)
+  end
+end
