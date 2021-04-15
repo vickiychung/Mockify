be43fc5ba05c317f75e537af9e146be70c2c24c4
vickiychung <vickiychung@gmail.com>
2021-04-14 23:05:22 -0700

    Add playlists to entities

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -3,12 +3,14 @@ import usersReducer from './users_reducer';
 import tracksReducer from './tracks_reducer';
 import albumsReducer from './albums_reducer';
 import artistsReducer from './artists_reducer';
+import playlistsReducer from './playlists_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   tracks: tracksReducer,
   albums: albumsReducer,
-  artists: artistsReducer
+  artists: artistsReducer,
+  playlists: playlistsReducer
 });
 
 export default entitiesReducer;
