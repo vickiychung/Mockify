1a193997175d89686bcbd99df8e15fe4b9bb9390
vickiychung <vickiychung@gmail.com>
2021-04-15 11:04:44 -0700

    Add icons and links to sidebar

--- frontend/components/side_bar/side_bar.jsx
+++ frontend/components/side_bar/side_bar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faBookOpen } from "@fortawesome/free-solid-svg-icons";
 
 class SideBar extends React.Component {
   render() {
@@ -8,6 +10,20 @@ class SideBar extends React.Component {
         <Link to="/">
           <img id="sidebar-logo" src={window.logoDarkURL} alt="mockify_logo"/>
         </Link>
+
+        <div className="sidebar-home">
+          <Link className="link" to="/">
+            <FontAwesomeIcon id="home-icon" icon={faHome} />
+            <p>Home</p>
+          </Link>
+        </div>
+
+        <div className="sidebar-library">
+          <Link className="link" to="/">
+            <FontAwesomeIcon id="library-icon" icon={faBookOpen} />
+            <p>Your Library</p>
+          </Link>
+        </div>
       </div>
     );
   }
