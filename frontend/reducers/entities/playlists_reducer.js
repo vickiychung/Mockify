18cb1fca25c81b63f7fa07d51b80354a1a485440
vickiychung <vickiychung@gmail.com>
2021-04-14 23:01:38 -0700

    Add playlistsReducer

---
+++ frontend/reducers/entities/playlists_reducer.js
@@ -0,0 +1,28 @@
+import {
+  RECEIVE_PLAYLISTS,
+  RECEIVE_PLAYLIST,
+  REMOVE_PLAYLIST
+} from '../../actions/playlists_actions';
+
+const playlistsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  let newState = Object.assign({}, oldState);
+
+  switch (action.type) {
+    case RECEIVE_PLAYLISTS:
+      return action.playlists;
+    
+    case RECEIVE_PLAYLIST:
+      newState[action.playlist.id] = action.playlist
+      return newState;
+    
+    case REMOVE_PLAYLIST:
+      delete newState[action.playlistId];
+      return newState;
+  
+    default:
+      return oldState;
+  }
+};
+
+export default playlistsReducer;
