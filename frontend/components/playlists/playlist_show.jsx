a9321053fb997a1ab1c73b5a89f0c8b64134738c
vickiychung <vickiychung@gmail.com>
2021-05-17 12:30:58 -0700

    Fix playlist styling

--- frontend/components/playlists/playlist_show.jsx
+++ frontend/components/playlists/playlist_show.jsx
@@ -23,8 +23,8 @@ class PlaylistShow extends React.Component {
           <p className="playlist-track-id">{idx + 1}</p>
           <p className="playlist-track-name">{track.name}</p>
           <p className="playlist-album-name">{albums[track.albumId]["title"]}</p>
-          <p>{track.artistName}</p>
-          <p className="track-length">{track.length.toFixed(2)}</p>
+          <p className="playlist-artist-name">{track.artistName}</p>
+          <p className="playlist-track-length">{track.length.toFixed(2)}</p>
         </li>
       ));
     }
@@ -36,17 +36,19 @@ class PlaylistShow extends React.Component {
         <div className="playlist-show-container">
           <h1>{playlist.name}</h1>
 
-          <div className="playlist-tracks-header">
-            <p>#</p>
-            <p>TITLE</p>
-            <p>ALBUM</p>
-            <p>ARTIST</p>
-            <p> <FontAwesomeIcon icon={faClock} /> </p>
-          </div>
+          <div className="playlist-tracks-wrapper">
+            <div className="playlist-tracks-header">
+              <p>#</p>
+              <p>TITLE</p>
+              <p>ALBUM</p>
+              <p>ARTIST</p>
+              <p> <FontAwesomeIcon icon={faClock} /> </p>
+            </div>
 
-          <ul className="tracks-container">
-            {list}
-          </ul>
+            <ul className="tracks-container">
+              {list}
+            </ul>
+          </div>
         </div>
       </div>
     );
