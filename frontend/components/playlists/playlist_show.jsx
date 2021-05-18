72454dc138d42c27143c5ab4e577ddd73b21b0d2
vickiychung <vickiychung@gmail.com>
2021-05-17 18:19:13 -0700

    Add delete track icon

--- frontend/components/playlists/playlist_show.jsx
+++ frontend/components/playlists/playlist_show.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { faClock, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 class PlaylistShow extends React.Component {
   componentDidMount() {
@@ -38,6 +38,8 @@ class PlaylistShow extends React.Component {
           </Link>
 
           <p className="playlist-track-length">{track.length.toFixed(2)}</p>
+
+          <p className="playlist-track-delete"> <FontAwesomeIcon icon={faTrashAlt} /> </p>
         </li>
       ));
     }
