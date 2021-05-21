533a4cc25b75c30e840e8ae5cc92d18aaf466503
vickiychung <vickiychung@gmail.com>
2021-05-20 21:49:36 -0700

    Add spacing to error messages

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -29,7 +29,7 @@ class SessionForm extends React.Component {
     return (
       <ul>
         {this.props.errors.map((error, i) => (
-          <li key={`error-${i}`}>
+          <li key={`error-${i}`} className="error-item">
             {error}
           </li>
         ))}
