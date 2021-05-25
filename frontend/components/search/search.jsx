724c5012b3f6a99b61679231ef3a9137a6a30ef7
vickiychung <vickiychung@gmail.com>
2021-05-25 11:59:25 -0700

    Add search component skeleton

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchTracks();
+  }
+
+  render() {
+    
+    return (
+
+    );
   }
 }
 
