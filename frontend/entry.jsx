63ba7244c65b504d10e9c6b82e45ad692e58286e
vickiychung <vickiychung@gmail.com>
2021-07-01 12:35:26 -0700

    Remove testing code

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-//TESTING
-// import { signUp, signIn, signOut } from './actions/session_actions';
-//TESTING
-
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
   let store;
@@ -24,15 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-
-  //TESTING
-  // window.signUp = signUp;
-  // window.signIn = signIn;
-  // window.signOut = signOut;
-
-  // window.getState = store.getState;
-  // window.dispatch = store.dispatch;
-  //TESTING
 
   ReactDOM.render(<Root store={store} />, root);
 });
